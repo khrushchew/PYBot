021c6dce2ed10f20570c4c5d2f4f391e11becd34
Popov Nikita <popovn2268@gmail.com>
2023-11-29 00:51:09 +0300

    Basic functionality

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
-import telebot
+# My import for a message
+from welcome_message import a
 
+# Import for a telebot
+import telebot
+import webbrowser
+
+# More and more import
+from random import choice
+
+bot = telebot.TeleBot('6691756516:AAHN4XIDmeIJ_Qk1dkDIYe97ce8hqE4MgGQ')
+
+
+@bot.message_handler(commands=['website'])
+def site(message):
+    webbrowser.open("https://www.kinopoisk.ru")
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, f'''Hi, {message.from_user.first_name}! {choice(a)}''')
+
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    with open('Help.txt', 'r', encoding='Utf-8') as help_file:
+        bot.send_message(message.chat.id, help_file.read())
+
+
+@bot.message_handler(content_types=['photo', 'video', 'audio', 'text'])
+def error(message):
+    bot.reply_to(message, "Error... Unknown command!")
+
+
+bot.infinity_polling()
---
+++ welcome_message.py
@@ -0,0 +1,40 @@
+a = [
+    "Hey there! Feeling a bit bored? How about adding some excitement to your day with a fantastic movie?\n"
+    "Our collection is full of hidden gems waiting to be discovered. Grab some popcorn and dive in!",
+
+    "Greetings, movie enthusiast! Looking for a cinematic escape? Well, you're in luck!\n"
+    "Take a break, explore our movie library, and let the magic of storytelling transport you to new worlds.",
+
+    "Hello, friend! Need a break from the daily hustle? Why not unwind with a great movie?\n"
+    "We've got a curated selection that caters to all tastes. Pick one, sit back, and enjoy the show!",
+
+    "How's your day going? If you're in the mood for some entertainment, we've got you covered!\n"
+    "Browse through our diverse movie options and turn your ordinary day into a cinematic adventure.",
+
+    "Hey you! Yes, you. Feeling like the day could use a dose of excitement?\n"
+    "Check out our movie recommendations, find something that piques your interest, and let the movie magic begin!",
+
+    "Salutations, film aficionado! Need a break from reality? Dive into our movie wonderland!\n"
+    "Pick a genre, pick a mood, and let the movie marathon commence. Your ticket to entertainment awaits!",
+
+    "Good day! Looking for a way to spice up your routine? How about a movie night?\n"
+    "Our selection has something for everyone—grab your favorite snacks and let the cinematic journey begin!",
+
+    "Greetings, fellow cinephile! Need a break from reality? Explore our movie treasury and find the perfect escape.\n"
+    "Movie nights are the best nights, don't you think?",
+
+    "Hey, movie buff! Feeling the need for some on-screen magic? Browse our movie haven and let the entertainment roll.\n"
+    "Your next favorite film might be just a click away!",
+
+    "Hello there! Life's too short for boring days. Shake things up with a captivating movie!\n"
+    "Our diverse collection promises a cinematic experience tailored just for you.",
+
+    "Hey friend! Tired of the same old routine? Spice things up with a cinematic adventure!\n"
+    "Grab a cozy blanket, choose a movie from our selection, and let the entertainment therapy begin.",
+
+    "Howdy! Long day, huh? Unwind with the magic of movies! Our carefully curated selection awaits your exploration.\n"
+    "Find a comfy spot, hit play, and let the film-induced relaxation commence!",
+
+    "Top of the day to you! Seeking a remedy for monotony? Dive into our movie library and discover a world of storytelling wonders.\n"
+    "Movie nights: because why settle for ordinary when you can have extraordinary?",
+]
