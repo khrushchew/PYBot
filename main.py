ed9a08d79a3f290c34206b810eb3f01ece75ab1f
Popov Nikita <popovn2268@gmail.com>
2023-11-29 19:46:21 +0300

    Rus Version

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from welcome_message import a
 # Import for a telebot
 import telebot
 import webbrowser
+from telebot import types
 
 # More and more import
 from random import choice
@@ -16,20 +17,32 @@ def site(message):
     webbrowser.open("https://www.kinopoisk.ru")
 
 
+# def on_click(message):
+#     if message.text == 'start':
+#         bot.send_message(message.chat.id, choice(a))
+
+
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f'''Hi, {message.from_user.first_name}! {choice(a)}''')
+    markup = types.ReplyKeyboardMarkup()
+    button_for_start = types.KeyboardButton('/start')
+    markup.add(button_for_start)
+    bot.send_message(message.chat.id, choice(a), reply_markup=markup)
+    # bot.register_next_step_handler(message, on_click)
 
 
 @bot.message_handler(commands=['help'])
 def main(message):
+    markup = types.ReplyKeyboardMarkup()
+    button_for_start = types.KeyboardButton('help')
+    markup.add(button_for_start)
     with open('Help.txt', 'r', encoding='Utf-8') as help_file:
         bot.send_message(message.chat.id, help_file.read())
 
 
 @bot.message_handler(content_types=['photo', 'video', 'audio', 'text'])
 def error(message):
-    bot.reply_to(message, "Error... Unknown command!")
+    bot.reply_to(message, "Ошибка... Неизвестная команда!")
 
 
 bot.infinity_polling()
--- welcome_message.py
+++ welcome_message.py
@@ -1,40 +1,40 @@
 a = [
-    "Hey there! Feeling a bit bored? How about adding some excitement to your day with a fantastic movie?\n"
-    "Our collection is full of hidden gems waiting to be discovered. Grab some popcorn and dive in!",
+    "Привет! Немного скучно? Что скажешь насчет того, чтобы добавить в свой день немного волнения с фантастическим фильмом?\n"
+    "Наша коллекция полна скрытых шедевров, ждущих своего открытия. Возьми попкорн и погрузись в мир кино!",
 
-    "Greetings, movie enthusiast! Looking for a cinematic escape? Well, you're in luck!\n"
-    "Take a break, explore our movie library, and let the magic of storytelling transport you to new worlds.",
+    "Привет, киноман! Ищешь кинематографическое убежище? Тогда тебе повезло!\n"
+    "Сделай перерыв, изучи нашу кинотеку, и дай волшебству рассказов перенести тебя в новые миры.",
 
-    "Hello, friend! Need a break from the daily hustle? Why not unwind with a great movie?\n"
-    "We've got a curated selection that caters to all tastes. Pick one, sit back, and enjoy the show!",
+    "Привет, друг! Нужен перерыв от повседневной суеты? Почему бы не расслабиться с отличным фильмом?\n"
+    "У нас есть огромный выбор на любой вкус. Выбери один, устройся поудобнее и наслаждайся шоу!",
 
-    "How's your day going? If you're in the mood for some entertainment, we've got you covered!\n"
-    "Browse through our diverse movie options and turn your ordinary day into a cinematic adventure.",
+    "Как проходит твой день? Если хочешь немного развлечения, у нас оно для тебя есть!\n"
+    "Просматривай наши разнообразные варианты фильмов и преврати свой обычный день в кинематографическое приключение.",
 
-    "Hey you! Yes, you. Feeling like the day could use a dose of excitement?\n"
-    "Check out our movie recommendations, find something that piques your interest, and let the movie magic begin!",
+    "Привет тебе! Да, именно тебе. Чувствуешь, что день мог бы быть более захватывающим?\n"
+    "Ознакомься с нашими рекомендациями по фильмам, найди что-то, что привлекает тебя, и пусть начнется волшебство кино!",
 
-    "Salutations, film aficionado! Need a break from reality? Dive into our movie wonderland!\n"
-    "Pick a genre, pick a mood, and let the movie marathon commence. Your ticket to entertainment awaits!",
+    "Приветствую, киноман! Нужен отдых от реальности? Погружайся в наше киночудо!\n"
+    "Выбери жанр, выбери настроение, и пусть начнется киномарафон. Твой билет в мир развлечений уже здесь!",
 
-    "Good day! Looking for a way to spice up your routine? How about a movie night?\n"
-    "Our selection has something for everyone—grab your favorite snacks and let the cinematic journey begin!",
+    "Добрый день! Ищешь способ разнообразить свою рутину? Что скажешь насчет кино-вечера?\n"
+    "У нас есть что-то для каждого — возьми свои любимые закуски и позволь кинематографическому путешествию начаться!",
 
-    "Greetings, fellow cinephile! Need a break from reality? Explore our movie treasury and find the perfect escape.\n"
-    "Movie nights are the best nights, don't you think?",
+    "Привет, собрат киноманов! Нужен отдых от реальности? Исследуй наш киноковчег и найди идеальное убежище.\n"
+    "Вечера с фильмами — лучшие вечера, не так ли?",
 
-    "Hey, movie buff! Feeling the need for some on-screen magic? Browse our movie haven and let the entertainment roll.\n"
-    "Your next favorite film might be just a click away!",
+    "Привет, киноман! Чувствуешь потребность в экранной магии? Просмотри нашу киноподборку и пусть развлечение начнется.\n"
+    "Возможно, твой следующий любимый фильм всего в паре кликов!",
 
-    "Hello there! Life's too short for boring days. Shake things up with a captivating movie!\n"
-    "Our diverse collection promises a cinematic experience tailored just for you.",
+    "Привет! Жизнь слишком коротка для скучных дней. Встряхни события с захватывающим фильмом!\n"
+    "Наша разнообразная коллекция обещает подарить незабываемые ощущения. Может быть у нас найдётся фильм созданный специально для тебя?",
 
-    "Hey friend! Tired of the same old routine? Spice things up with a cinematic adventure!\n"
-    "Grab a cozy blanket, choose a movie from our selection, and let the entertainment therapy begin.",
+    "Привет, друг! Устал от не заканчивающейся рутины? Придай остроты своей жизни с помощью кинематографического приключения!\n"
+    "Возьми уютное одеяло, выбери фильм из нашей подбоки и пусть начнется терапия развлечением.",
 
-    "Howdy! Long day, huh? Unwind with the magic of movies! Our carefully curated selection awaits your exploration.\n"
-    "Find a comfy spot, hit play, and let the film-induced relaxation commence!",
+    "Привет! Длинный день, не так ли? Расслабься с помощью волшебства кино! У нас для тебя тщательно подобрано разнообразие фильмов.\n"
+    "Найди уютное место, выбери нужный фильм и дай начало релаксации под влиянием фильмов!",
 
-    "Top of the day to you! Seeking a remedy for monotony? Dive into our movie library and discover a world of storytelling wonders.\n"
-    "Movie nights: because why settle for ordinary when you can have extraordinary?",
+    "Приветствую! Устал от повседневной рутины? Погрузись в наш мир кино и открой для себя удивительные истории.\n"
+    "Вечера с фильмами - ведь почему бы не сделать их необычными, вместо того чтобы придерживаться обыденности?"
 ]
